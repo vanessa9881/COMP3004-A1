edb418b0cb41ddb3c78bb561900103a5842b6cc0
Vanessa <vanessa@dhcp-203-51.eduroam.carleton.ca>
2018-09-13 12:34:07 -0400

    Maven project created and junit dependency added to pom.xml

---
+++ A1/src/main/java/comp3004/A1/App.java
@@ -0,0 +1,13 @@
+package comp3004.A1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
