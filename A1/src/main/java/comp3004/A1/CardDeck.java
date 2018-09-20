c4669c9fe5828070cae7d66cd489c74fb5824c22
Vanessa <vanessa@Yarlies-MacBook-Pro.local>
2018-09-19 23:13:38 -0400

    Test: JQKas10Test added

--- A1/src/main/java/comp3004/A1/CardDeck.java
+++ A1/src/main/java/comp3004/A1/CardDeck.java
@@ -3,7 +3,7 @@ package comp3004.A1;
 public class CardDeck {
 	int[] cdeck = new int[52];
 	String[] suits = {"S", "H", "D", "C"};
-    String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
     
 
 	public int total() {
---
+++ A1/src/test/java/comp3004/A1/JQKas10Test.java
@@ -0,0 +1,12 @@
+package comp3004.A1;
+
+import junit.framework.TestCase;
+
+public class JQKas10Test extends TestCase {
+	
+	public void testJQKas10Test () {
+		CardDeck tester = new CardDeck ();
+		
+assertEquals('10', 'J');	}
+
+}
