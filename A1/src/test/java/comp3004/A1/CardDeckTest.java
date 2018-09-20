ce63c81cb635e385c580639bf2b474fa67f5caa8
Vanessa <vanessa@Yarlies-MacBook-Pro.local>
2018-09-19 21:02:04 -0400

    Added Card Deck Test

---
+++ A1/src/test/java/comp3004/A1/CardDeckTest.java
@@ -0,0 +1,16 @@
+package comp3004.A1;
+
+import junit.framework.TestCase;
+
+public class CardDeckTest extends TestCase {
+	
+	public void FileInput() {
+		FileConsole indicator = new FileConsole 
+		
+		
+		
+				
+		
+	}
+
+}
